08546911c4d79f1880a018c3c83fda5a28bc32c5
Rudy <rudy.dev.kor@gmail.com>
2021-04-22 12:32:13 +0900

    Move some functions into utils module
     - Clean up code

--- infer_camera.py
+++ infer_camera.py
@@ -5,46 +5,7 @@ import cv2
 import numpy as np
 
 import image_selection
-
-def cbox2bbox(cbox):
-    w_half = cbox[2] / 2
-    h_half = cbox[3] / 2
-    x1 = cbox[0] - w_half
-    y1 = cbox[1] - h_half
-    x2 = cbox[0] + w_half
-    y2 = cbox[1] + h_half
-    return [x1, y1, x2, y2]
-
-def bbox2cbox(bbox):
-    w = bbox[2] - bbox[0]
-    h = bbox[3] - bbox[1]
-    cx = bbox[0] - w / 2
-    cy = bbox[1] - h / 2
-    return [cx, cy, w, h]
-
-def calc_iou(bb1, bb2):
-	# determine the (x, y)-coordinates of the intersection rectangle
-	xA = max(bb1[0], bb2[0])
-	yA = max(bb1[1], bb2[1])
-	xB = min(bb1[2], bb2[2])
-	yB = min(bb1[3], bb2[3])
-
-	# compute the area of intersection rectangle
-	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-
-	# compute the area of both the prediction and ground-truth
-	# rectangles
-	boxAArea = (bb1[2] - bb1[0] + 1) * (bb1[3] - bb1[1] + 1)
-	boxBArea = (bb2[2] - bb2[0] + 1) * (bb2[3] - bb2[1] + 1)
-
-	# compute the intersection over union by taking the intersection
-	# area and dividing it by the sum of prediction + ground-truth
-	# areas - the interesection area
-	iou = interArea / float(boxAArea + boxBArea - interArea)
-
-	# return the intersection over union value
-	return iou
-
+import utils
 
 # def detect(net, output_names, img):
 #     blob = cv2.dnn.blobFromImage(img, 1./255.)
@@ -122,53 +83,14 @@ def detect(net, output_names, img, score_thresh=0.5):
 
     return idxs, boxes, confidences, classIDs
 
-def draw_rect(img, bb, size, color):
-    x = bb[0] - bb[2] // 2 - 5
-    y = bb[1] - bb[3] // 2 - 5
-    w = bb[2] + 10
-    h = bb[3] + 10
-    x = x if x >= 0 else 0
-    y = y if y >= 0 else 0
-    w = w if x+w < size[0] else size[0]-x
-    h = h if y+h < size[1] else size[1]-y
-    cv2.rectangle(img, (x,y), (x+w,y+h), color,4)
-
-    return img
-
-
-def crop_image(image, roi):
-    #print roi
-    clone = image.copy()
-    retImg = clone[roi[0][1]:roi[1][1], roi[0][0]:roi[1][0]]
-    return retImg
-
-
-def combine_result(idxs, boxes, class_ids, labels):
-
-    x_and_class_list = []
-
-    if len(idxs) <= 0:
-        return ''
-
-    for i in idxs.flatten():
-        x_and_class_list.append((boxes[i][0], labels[class_ids[i]]))
-
-    x_and_class_str_list = sorted(x_and_class_list, key=lambda tup: tup[0])
-
-    result = ''
-    for _, class_str in x_and_class_str_list:
-        result += class_str
-
-    return result
 
 def detect_rois(image, roi_points, roi_size_wh, net, output_names, 
                 score_thresh):
     
     idxs_list, boxes_list, scores_list, class_ids_list = [], [], [] ,[]
     for roi in range(int(len(roi_points)/2)):
-        sel = crop_image(image.copy(),
-                         [roi_points[roi*2],
-                          roi_points[2*roi+1]])
+        sel = utils.crop_image(image.copy(), [roi_points[roi*2],
+                                              roi_points[2*roi+1]])
 
         sel = cv2.resize(sel, (roi_size_wh[0], roi_size_wh[1]))
         idxs, boxes, scores, class_ids = detect(net, output_names, 
@@ -231,8 +153,8 @@ def detect_video_with_roi(cfg, weights, video_path, video_size_wh, roi_size_wh,
                                                 idxs_list, boxes_list, 
                                                 scores_list, class_ids_list):
 
-            sel = crop_image(frame.copy(), [roi_points[r*2], 
-                                            roi_points[r*2+1]])
+            sel = utils.crop_image(frame.copy(), [roi_points[r*2], 
+                                                  roi_points[r*2+1]])
 
             sel = cv2.resize(sel, (roi_size_wh[0], roi_size_wh[1]))
 
@@ -266,7 +188,8 @@ def detect_video_with_roi(cfg, weights, video_path, video_size_wh, roi_size_wh,
                         0.5, color, 1)
                     cv2.imshow(f'panel_{r}', sel)
 
-            combined = combine_result(idxs, boxes, class_ids, labels=labels)
+            combined = utils.combine_result(idxs, boxes, class_ids, labels)
+
             cv2.rectangle(frame, roi_points[r*2], roi_points[r*2+1],
                           (0,255,0), 2)
 
@@ -277,7 +200,7 @@ def detect_video_with_roi(cfg, weights, video_path, video_size_wh, roi_size_wh,
         fps = 1/(time.time()-start_time)
         fps = str(int(fps)) + ' fps'
 
-        cv2.putText(frame, fps, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+        cv2.putText(frame, fps, (5, 50), cv2.FONT_HERSHEY_SIMPLEX,  
             1, (100, 255, 0), 2, cv2.LINE_AA)
         cv2.imshow('frame', frame)
         cv2.waitKey()
@@ -325,9 +248,10 @@ def detect_video(cfg, weights, video_path, video_size_wh, output_video_path):
             fps = str(int(fps)) + ' fps'
             for b in boxes:
                 b = np.array(b, dtype=np.int32)
-                img = draw_rect(img, b, video_size_wh, (0, 0, 255))
+                img = utils.draw_rect(img, b, video_size_wh, (0, 0, 255))
 
-            cv2.putText(img, fps, (5, 50), cv2.FONT_HERSHEY_SIMPLEX , 1, (100, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(img, fps, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                        (100, 255, 0), 2, cv2.LINE_AA)
 
             cv2.imshow('results', img)
 
