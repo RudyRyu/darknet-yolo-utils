01585288b225f094c87fd12be159f098f128cc9f
Rudy <rudy.dev.kor@gmail.com>
2021-05-26 14:49:32 +0900

    Add darknet2tf package

---
+++ darknet2tf/save_utils.py
@@ -0,0 +1,155 @@
+import numpy as np
+import tensorflow as tf
+
+
+def decode_tf(conv_output, output_size_wh, 
+              num_class, anchors, num_anchor, strides, xyscale,
+              i=0):
+
+    batch_size = tf.shape(conv_output)[0]
+
+    conv_output = tf.reshape(conv_output,
+        (batch_size, *output_size_wh, num_anchor, num_class+5))
+
+    conv_raw_dxdy, conv_raw_dwdh, conv_raw_conf, conv_raw_prob = \
+        tf.split(conv_output, (2, 2, 1, num_class), axis=-1)
+
+    xy_grid = tf.meshgrid(
+        tf.range(output_size_wh[1]), tf.range(output_size_wh[0]))
+    xy_grid = tf.expand_dims(tf.stack(xy_grid, axis=-1), axis=2)  # [gx, gy, 1, 2]
+    xy_grid = tf.tile(
+        tf.expand_dims(xy_grid, axis=0), [batch_size, 1, 1, num_anchor, 1])
+
+    xy_grid = tf.cast(xy_grid, tf.float32)
+
+    pred_xy = ((tf.sigmoid(conv_raw_dxdy)*xyscale[i]) \
+                - 0.5*(xyscale[i]-1)+xy_grid) * strides[i]
+
+    pred_wh = (tf.exp(conv_raw_dwdh) * anchors[i])
+    pred_xywh = tf.concat([pred_xy, pred_wh], axis=-1)
+
+    pred_conf = tf.sigmoid(conv_raw_conf)
+    pred_prob = tf.sigmoid(conv_raw_prob)
+
+    pred_prob = pred_conf * pred_prob
+    pred_prob = tf.reshape(pred_prob, (batch_size, -1, num_class))
+    pred_xywh = tf.reshape(pred_xywh, (batch_size, -1, 4))
+
+    return pred_xywh, pred_prob
+
+
+def filter_boxes(box_xywh, scores, 
+                 score_threshold=0.4, 
+                 input_shape=tf.constant([128,64])):
+
+    scores_max = tf.math.reduce_max(scores, axis=-1)
+
+    mask = scores_max >= score_threshold
+    class_boxes = tf.boolean_mask(box_xywh, mask)
+    pred_conf = tf.boolean_mask(scores, mask)
+    class_boxes = tf.reshape(class_boxes, [tf.shape(scores)[0], -1, tf.shape(class_boxes)[-1]])
+    pred_conf = tf.reshape(pred_conf, [tf.shape(scores)[0], -1, tf.shape(pred_conf)[-1]])
+
+    box_xy, box_wh = tf.split(class_boxes, (2, 2), axis=-1)
+
+    input_shape = tf.cast(input_shape, dtype=tf.float32)
+
+    box_yx = box_xy[..., ::-1]
+    box_hw = box_wh[..., ::-1]
+
+    box_mins = (box_yx - (box_hw / 2.)) / input_shape
+    box_maxes = (box_yx + (box_hw / 2.)) / input_shape
+    boxes = tf.concat([
+        box_mins[..., 0:1],  # y_min
+        box_mins[..., 1:2],  # x_min
+        box_maxes[..., 0:1],  # y_max
+        box_maxes[..., 1:2]  # x_max
+    ], axis=-1)
+    # return tf.concat([boxes, pred_conf], axis=-1)
+    return (boxes, pred_conf)
+
+
+def load_weights(model, weights_file, layer_size, output_pos):
+    wf = open(weights_file, 'rb')
+    major, minor, revision, seen, _ = np.fromfile(wf, dtype=np.int32, count=5)
+
+    j = 0
+    for i in range(layer_size):
+        conv_layer_name = 'conv2d_%d' %i if i > 0 else 'conv2d'
+        bn_layer_name = 'batch_normalization_%d' %j if j > 0 else 'batch_normalization'
+
+        conv_layer = model.get_layer(conv_layer_name)
+        filters = conv_layer.filters
+        k_size = conv_layer.kernel_size[0]
+        in_dim = conv_layer.input_shape[-1]
+
+        if i not in output_pos:
+            # darknet weights: [beta, gamma, mean, variance]
+            bn_weights = np.fromfile(wf, dtype=np.float32, count=4 * filters)
+            # tf weights: [gamma, beta, mean, variance]
+            bn_weights = bn_weights.reshape((4, filters))[[1, 0, 2, 3]]
+            bn_layer = model.get_layer(bn_layer_name)
+            j += 1
+        else:
+            conv_bias = np.fromfile(wf, dtype=np.float32, count=filters)
+
+        # darknet shape (out_dim, in_dim, height, width)
+        conv_shape = (filters, in_dim, k_size, k_size)
+        conv_weights = np.fromfile(wf, dtype=np.float32, count=np.product(conv_shape))
+        # tf shape (height, width, in_dim, out_dim)
+        conv_weights = conv_weights.reshape(conv_shape).transpose([2, 3, 1, 0])
+
+        if i not in output_pos:
+            conv_layer.set_weights([conv_weights])
+            bn_layer.set_weights(bn_weights)
+        else:
+            conv_layer.set_weights([conv_weights, conv_bias])
+
+    # assert len(wf.read()) == 0, 'failed to read all data'
+    wf.close()
+
+
+def get_tf_model(input_layer, feature_maps, input_size_wh, output_sizes_wh,
+                 num_class, anchors, num_anchor, strides, xyscale,
+                 score_thresh):
+    
+    bbox_tensors = []
+    prob_tensors = []
+    for i, fm in enumerate(feature_maps):
+        bbox, prob = decode_tf(fm, output_sizes_wh[i], 
+            num_class, anchors, num_anchor, strides, xyscale)
+
+        bbox_tensors.append(bbox)
+        prob_tensors.append(prob)
+
+    pred_bbox = tf.concat(bbox_tensors, axis=1)
+    pred_prob = tf.concat(prob_tensors, axis=1)
+
+    boxes, pred_conf = filter_boxes(pred_bbox, pred_prob, 
+        score_threshold=score_thresh, 
+        input_shape=tf.constant(input_size_wh))
+
+    pred = tf.concat([boxes, pred_conf], axis=-1)
+
+    model = tf.keras.Model(input_layer, pred)
+
+    return model
+
+def save_tf(get_model_func, weights_file, save_output_dir, 
+            layer_size, output_pos,
+            input_size_wh, output_sizes_wh,
+            num_class, anchors, num_anchor, strides, xyscale,
+            score_thresh):
+
+    input_layer = tf.keras.layers.Input([*input_size_wh, 3])
+    feature_maps = get_model_func(input_layer)
+
+    model = get_tf_model(
+        input_layer, feature_maps, input_size_wh, output_sizes_wh, 
+        num_class, anchors, num_anchor, strides, xyscale,
+        score_thresh)
+
+    load_weights(model, weights_file, layer_size, output_pos)
+    model.summary()
+    model.save(save_output_dir)
+
---
+++ darknet2tf/sh_digit.py
@@ -0,0 +1,71 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+import numpy as np
+import tensorflow as tf
+
+import save_utils
+import common
+
+
+STRIDES = np.array([16])
+NUM_ANCHOR = 6
+ANCHORS = np.array([7,7, 13,31, 21,45, 25,32, 28,48, 38,43]).reshape(1,6,2)
+NUM_CLASS = 16
+XYSCALE = [1]
+INPUT_SIZE_WH = (128, 64)
+OUTPUT_SIZES_WH = [(INPUT_SIZE_WH[0]//8, INPUT_SIZE_WH[1]//8)]
+FILTER_MULTIPLIER = 1
+SCORE_THRESH = 0.4
+LAYER_SIZE = 10
+OUTPUT_POS = [9]
+
+
+def get_model_outputs(input_data):
+    f = FILTER_MULTIPLIER
+
+    x = common.convolutional(input_data, (3, 3, 3, 32*f))
+    x = tf.keras.layers.MaxPool2D(2, 2, 'same')(x)
+    x = common.convolutional(x, (3, 3, 32*f, 64*f))
+    x = tf.keras.layers.MaxPool2D(2, 2, 'same')(x)
+    x = common.convolutional(x, (3, 3, 64*f, 64*f))
+    x = common.convolutional(x, (1, 1, 64*f, 32*f))
+    x = common.convolutional(x, (3, 3, 32*f, 64*f))
+
+    route1 = x
+    route1 = common.reorg(route1, 2)
+
+    x = tf.keras.layers.MaxPool2D(2, 2, 'same')(x)
+    x = common.convolutional(x, (3, 3, 64*f, 128*f))
+    x = common.convolutional(x, (1, 1, 128*f, 64*f))
+    x = common.convolutional(x, (3, 3, 64*f, 128*f))
+
+    x = tf.concat([x, route1], axis=-1)
+    x = common.convolutional(x, (3, 3, 384*f, 128*f))
+    
+    out1 = common.convolutional(
+        x, (1, 1, 128*f, (NUM_CLASS+5)*NUM_ANCHOR),
+        activate=False, bn=False)
+
+    return [out1]
+
+if __name__ == '__main__':
+
+    params = {
+        'get_model_func': get_model_outputs,
+        'weights_file': 'cfg/digit/sh_digit_best.weights',
+        'save_output_dir': 'sh_digit/checkpoint/',
+        'layer_size': LAYER_SIZE,
+        'output_pos': OUTPUT_POS,
+        'input_size_wh': INPUT_SIZE_WH,
+        'output_sizes_wh': OUTPUT_SIZES_WH,
+        'num_class': NUM_CLASS, 
+        'anchors': ANCHORS,
+        'num_anchor': NUM_ANCHOR,
+        'strides': STRIDES,
+        'xyscale': XYSCALE,
+        'score_thresh': SCORE_THRESH
+    }
+
+    save_utils.save_tf(**params)
